7b5ba19ea8531f6d701ff8d9751f426c8f7a6bae
Zoltan Kiraly <raketa001@gmail.com>
2017-10-03 16:32:53 +0000

    Added Book model, Book admin class, changed store template

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'stock')
+
+admin.site.register(Book, BookAdmin)
